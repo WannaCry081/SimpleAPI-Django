fc77aaca78a7f8f8bb45ea023264a138eec79a83
Lirae Que Data <liraedata59@gmail.com>
2024-02-06 00:42:54 +0800

    Create note model in notes.py

---
+++ core/models/notes.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Note(models.Model):
+    
+    user_id = models.ForeignKey("User", on_delete = models.CASCADE, blank = False)
+    
+    title = models.CharField(max_length = 100, unique = True, blank = False)
+    body = models.TextField(blank = True, null = True)
+    
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)    
+
+    REQUIRED_FIELDS = ["title"]
+
+    
+    def __str__(self):
+        return self.title
+    
+    
