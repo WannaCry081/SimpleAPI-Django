e56ebe4041ef7c47bae05a774febb67b6ad1c5bd
Lirae Que Data <liraedata59@gmail.com>
2024-02-06 02:37:54 +0800

    Add to_representation method in note_serializer.py

--- core/api/v1/serializers/note_serializer.py
+++ core/api/v1/serializers/note_serializer.py
@@ -3,11 +3,22 @@ from core.models import Note
 
 class NoteSerializer(serializers.ModelSerializer):
     
+    user = serializers.StringRelatedField()
+    
     class Meta:
         
         model = Note
-        fields = ["id", "title", "body"]
+        fields = ["id", "title", "body", "user"]
         read_only_fields = ("created_at", "updated_at")
         
-            
+    
+    def get_user(self, instance):
+        pass
+    
+    
+    def to_representation(self, instance):
+        
+        data = super().to_representation(instance)
+        data["user"] = self.get_user(instance)
+        return data
     
