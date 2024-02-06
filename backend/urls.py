962c244fc2ed2ee6a3eed577da051c1cce96bf4c
Lirae Que Data <liraedata59@gmail.com>
2024-02-06 17:41:15 +0800

    Add core api v1 routing

--- backend/urls.py
+++ backend/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/', include([
+        
+        # API v1.0.0
+        path('v1/', include('core.api.v1.urls'))
+    ]))
+
 ]
