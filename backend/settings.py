75415ee35c3c38788a7776f427b62a75667324bc
Lirae Que Data <liraedata59@gmail.com>
2024-02-09 01:03:08 +0800

    Update REST_FRAMEWORK default renderers in settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -141,6 +141,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES' : [
         'rest_framework.permissions.IsAuthenticated'
     ], 
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
     'DEFAULT_FILTER_BACKENDS' : [
         'rest_framework.filters.OrderingFilter',
         'rest_framework.filters.SearchFilter'
