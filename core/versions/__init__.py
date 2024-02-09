d2a31565420a4f4edc6bb79c71148140a9764c85
Lirae Que Data <liraedata59@gmail.com>
2024-02-09 18:00:42 +0800

    Add modern version export

--- core/versions/__init__.py
+++ core/versions/__init__.py
@@ -1,6 +1,8 @@
 from core.versions.legacy_version import LegacyAPIVersion
+from core.versions.modern_version import ModernAPIVersion
 
 
 __all__ = [
-    "LegacyAPIVersion"
+    "LegacyAPIVersion",
+    "ModernAPIVersion"
 ]
