feac593879c8596564b5cc533460d0047d9c08d6
Lirae Que Data <liraedata59@gmail.com>
2024-02-09 23:19:41 +0800

    Update partial_update method in note_viewset.py

--- core/api/v1/viewsets/note_viewsets.py
+++ core/api/v1/viewsets/note_viewsets.py
@@ -7,7 +7,6 @@ from core.api.v1.serializers import NoteSerializer, UserSerializer
 from core.models import Note
 from core.versions import LegacyAPIVersion
 
-from rest_framework.decorators import action
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
@@ -30,24 +29,31 @@ class NoteViewSet(viewsets.GenericViewSet,
     ordering = ["-created_at"]
     throttle_classes = [UserRateThrottle]
     
-    
+    @swagger_auto_schema(
+        operation_summary="List all notes for the authenticated user",
+        operation_description="This endpoint returns a list of notes for the authenticated user.",
+        responses={
+            status.HTTP_200_OK: openapi.Response("Ok", NoteSerializer(many=True))
+        },
+        produces = ["application/json", "application/xml", "text/html"]
+    )
     def list(self, request, *args, **kwargs):
         notes = Note.objects.filter(user_id=request.user)
 
         page = self.paginate_queryset(notes)
         if page is not None:
-            serializer = NoteSerializer(page, many=True).data
+            serializer = self.get_serializer(page, many=True).data
             return self.get_paginated_response(serializer)
 
-        serializer = NoteSerializer(notes, many=True).data
+        serializer = self.get_serializer(notes, many=True).data
         return Response(serializer, status = status.HTTP_200_OK)
         
     def create(self, request, *args, **kwargs):
-        serializer = NoteSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         
         note = serializer.save(user_id=request.user)
-        response_data = NoteSerializer(note).data
+        response_data = self.get_serializer(note).data
         
         return Response(response_data, status=status.HTTP_201_CREATED)
     
@@ -57,9 +63,14 @@ class NoteViewSet(viewsets.GenericViewSet,
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
     
-    
     def partial_update(self, request, *args, **kwargs):
-        return super().partial_update(request, *args, **kwargs)
+        
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception = True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
